bcdb32b16951d6816e2ba1c91bf2981764251c8e
flo7716 <florian.andre.77@gmail.com>
2026-09-11 11:37:05 +0200

    ajout route test

--- mon-api/index.js
+++ mon-api/index.js
@@ -8,4 +8,8 @@ app.get("/", (req, res) => {
   res.json({ message: "ça marche" })
 })
 
+app.get("/test", (req, res) => {
+  res.json({ message: "ça marche pour la route test" })
+})
+
 app.listen(3000, () => console.log("http://localhost:3000"))
