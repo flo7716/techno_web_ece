96c4430c846ff72046307ed91620c3e19f462c97
flo7716 <florian.andre.77@gmail.com>
2026-09-11 11:47:56 +0200

    ajout routes id et name

--- mon-api/index.js
+++ mon-api/index.js
@@ -12,4 +12,16 @@ app.get("/test", (req, res) => {
   res.json({ message: "ça marche pour la route test" })
 })
 
+app.get("/test/:id", (req, res) => {
+  console.log(req.params.id)
+  res.json({ message: `ça marche pour la route test avec l'id ${req.params.id}` })
+})
+
+
+app.get("/name/:name", (req, res) => {
+    console.log(req.params.name)
+    res.json({ message: 'ceci est la page de ' + req.params.name })
+})
+
+
 app.listen(3000, () => console.log("http://localhost:3000"))
