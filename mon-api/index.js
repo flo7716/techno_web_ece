45574af6878f3261497dc69a796ae93d6b9fb318
flo7716 <florian.andre.77@gmail.com>
2026-09-21 16:32:22 +0200

    remplacement fetch

--- mon-api/index.js
+++ mon-api/index.js
@@ -12,9 +12,15 @@ app.get("/test", (req, res) => {
   res.json({ message: "ça marche pour la route test" })
 })
 
-app.get("/test/:id", (req, res) => {
-  console.log(req.params.id)
-  res.json({ message: `ça marche pour la route test avec l'id ${req.params.id}` })
+app.get("/fetch", async (req, res) => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const data = await response.json()
+
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la récupération des données" })
+  }
 })
 
 
