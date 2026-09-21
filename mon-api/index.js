33c9aed58d880ce564bb995ed08616a69183a989
flo7716 <florian.andre.77@gmail.com>
2026-09-21 16:13:00 +0200

    debug index.js

--- mon-api/index.js
+++ mon-api/index.js
@@ -23,27 +23,5 @@ app.get("/name/:name", (req, res) => {
     res.json({ message: 'ceci est la page de ' + req.params.name })
 })
 
-<<<<<<< Updated upstream
-app.get("/name/:name/:age", (req, res) => {
-    console.log(req.params.name)
-    console.log(req.params.age)
-    res.json({ message: 'ceci est la page de ' + req.params.name + ' et il a ' + req.params.age + ' ans' })
-})
-
-app.get("/lien", (req, res) => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    res.json({ message: "ça marche pour la route lien" })
-})
-
-app.get("/lien/:id", (req, res) => {
-    fetch(`https://jsonplaceholder.typicode.com/todos/${req.params.id}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    res.json({ message: "ça marche pour la route lien et l'id est " + req.params.id })
-})
-=======
->>>>>>> Stashed changes
 
 app.listen(3000, () => console.log("http://localhost:3000"))
